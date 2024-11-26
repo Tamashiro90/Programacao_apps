96945c10571e10cbdbecff49d6cae8305f4e15d5
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-11-25 21:41:14 -0300

    Add files via upload

---
+++ Exercícios_Java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int numero = 5;
+		String texto = "10";
+		int result = numero + Integer.parseInt(texto);
+		System.out.println("O resultado é: " + result);
+	}
+}
